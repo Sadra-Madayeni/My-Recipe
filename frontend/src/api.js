1b6311e87b8b048c29f250d74d445792d1c1e298
Sadra <sadra.madayeni@gmail.com>
2026-06-11 21:01:25 +0330

    fix: point frontend API to live Back4App backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,38 @@
+// import axios from 'axios'
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:8000/api/',
+// })
+ 
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('access')
+//     if (token) {
+
+//       config.headers.Authorization = `Bearer ${token}`
+//     }
+//     return config
+//   },
+//   (error) => {
+//     return Promise.reject(error)
+//   }
+// )
+
+// export default api
+
+
+
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  
+  baseURL: 'https://culinary-backend.b4a.run/api/', 
 })
  
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('access')
     if (token) {
-
       config.headers.Authorization = `Bearer ${token}`
     }
     return config
